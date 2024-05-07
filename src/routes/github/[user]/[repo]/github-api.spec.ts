9e2bc8dfad7c71bea104a9bbdd11dac0bbe42eaa
Eitan R. <eitan.rosenberg@nytimes.com>
2024-05-07 13:02:50 -0400

    Add snapshot test

--- src/routes/github/[user]/[repo]/github-api.spec.ts
+++ src/routes/github/[user]/[repo]/github-api.spec.ts
@@ -1,11 +1,16 @@
-import { describe, it } from "vitest";
+import { describe, it, expect } from "vitest";
+import { GithubApi } from "./github-api";
 
 // Filename
 describe("github-api", () => {
   // Function name
   describe("getRepository", () => {
     // Every 'it' is a Product Requirement
-    it.todo("should return repository information");
+    it("should return repository information", async () => {
+      const api = new GithubApi(undefined);
+      const response = await api.getRepository("mhevery", "qwik");
+      expect(response).toMatchSnapshot();
+    });
     it.todo("should return timeout after x seconds with time out response");
   });
 });
