50fb52a2c44953d2258e3f58c0e6118a295e519f
Eitan R. <eitan.rosenberg@nytimes.com>
2024-05-08 10:11:20 -0400

    Update github-api.spec.ts

--- src/routes/github/[user]/[repo]/github-api.spec.ts
+++ src/routes/github/[user]/[repo]/github-api.spec.ts
@@ -1,29 +1,14 @@
 import { describe, it, vi } from "vitest";
 import { Fetch, GithubApi } from "./github-api";
 
-function mockPromise<T>() {
-  let resolve;
-  let reject;
-
-  const promise = new Promise((res, rej) => {
-    resolve = res;
-    reject = rej;
-  }) as Promise<T> & { resolve: typeof resolve; reject: typeof reject };
-  promise.resolve = resolve;
-  promise.reject = reject;
-  return promise;
-}
-
-// Filename
 describe("github-api", () => {
-  // Function name
   describe("getRepository", () => {
-    // Every 'it' is a Product Requirement
     it("should return repository information", async ({ expect }) => {
       const fetchMock = vi.fn<Parameters<Fetch>, ReturnType<Fetch>>(
         mockPromise
       );
-      const api = new GithubApi("TOKEN", fetchMock);
+      const api = new GithubApi("TOKEN", fetchMock, vi.fn(mockPromise) as any);
+
       const responsePromise = api.getRepository("USERNAME", "REPOSITORY");
 
       expect(fetchMock).toHaveBeenCalledWith(
@@ -36,9 +21,48 @@ describe("github-api", () => {
           },
         }
       );
-      fetchMock.mock.results[0].value.resolve!(new Response('"RESPONSE"'));
+      fetchMock.mock.results[0].value.resolve(new Response('"RESPONSE"'));
       expect(await responsePromise).toEqual("RESPONSE");
     });
-    it.todo("should return timeout after x seconds with time out response");
+
+    it("should timeout after x seconds with time out response", async ({
+      expect,
+    }) => {
+      const fetchMock = vi.fn<Parameters<Fetch>, ReturnType<Fetch>>(
+        mockPromise
+      );
+      const delayMock = vi.fn<[number], Promise<void>>(mockPromise);
+      const api = new GithubApi("TOKEN", fetchMock, delayMock);
+
+      const responsePromise = api.getRepository("USERNAME", "REPOSITORY");
+
+      expect(fetchMock).toHaveBeenCalledWith(
+        "https://api.github.com/repos/USERNAME/REPOSITORY",
+        {
+          headers: {
+            "User-Agent": "Qwik Workshop",
+            "X-GitHub-Api-Version": "2022-11-28",
+            Authorization: "Bearer TOKEN",
+          },
+        }
+      );
+      expect(delayMock).toHaveBeenCalledWith(4000);
+      delayMock.mock.results[0].value.resolve();
+      expect(await responsePromise).toEqual({
+        response: "timeout",
+      });
+    });
   });
 });
+
+function mockPromise<T>() {
+  let resolve!: (value: T) => void;
+  let reject!: (error: any) => void;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  }) as Promise<T> & { resolve: typeof resolve; reject: typeof reject };
+  promise.resolve = resolve;
+  promise.reject = reject;
+  return promise;
+}
